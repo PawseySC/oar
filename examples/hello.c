e314dfee7967a76ac70d2b8c36a1e736f42ab3ae
Maciej Cytowski <maciej.cytowski@pawsey.org.au>
2021-07-26 23:47:42 +0800

    adding aggregation

--- examples/hello.c
+++ examples/hello.c
@@ -1,35 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 #include <math.h>
 
-#define MAX 1000000
+#define MAX 1024*1024
 
-void second(double *res)
+void second(double *res,unsigned int *seed)
 {
 
   int i;
-  long max=1000000;
   
   #pragma omp parallel for
-  for(i=0;i<max;i++)
-    res[i]=sin((double)(i/1000))+cos((double)(i/10000));
+  for(i=0;i<MAX;i++)
+    res[i]=sin((double)(rand_r(seed)/1000))+cos((double)(rand_r(seed)/10000));
 
 }
 
 int main()
 {
   double *res;
-
+  unsigned int seed;
   res=(double*)malloc(MAX*sizeof(double));
+
+  srand(seed);
 
   #pragma omp parallel 
   {
     printf("Hello from thread %i of %i!\n", omp_get_thread_num(), omp_get_num_threads());
   }
 
-  second(res);
-  second(res);
-  second(res);
+  second(res,&seed);
+  second(res,&seed);
+  second(res,&seed);
 
   free(res);
 
