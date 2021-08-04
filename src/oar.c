3faa1d5eb681fec5237eda06048343315315db5a
Maciej Cytowski <maciej.cytowski@pawsey.org.au>
2021-08-04 22:44:11 +0800

    adding comments in the code

--- src/oar.c
+++ src/oar.c
@@ -21,11 +21,16 @@ do{                                                           \
 
 #define register_callback(name) register_callback_t(name, name##_t)
 
+#define MAX_REGIONS 128
+
+/* recipe - storing data read from recipe file in run mode */
 struct recipe {
   const void *codeptr;
   int nworkers;
 };
 
+/* perfdata - storing raw data for parallel regions, format used 
+              for sending data to Python wrapper via named pipe */
 struct perfdata {
   int id;
   int workers;
@@ -34,35 +39,33 @@ struct perfdata {
   long end;
 };
 
+/* global variables */
 int rid;
 int fd;
 int region;
 char *fifo="./pipe";
-struct perfdata perfdata_parallel[128];
-struct recipe recipe_parallel[128];
+struct perfdata perfdata_parallel[MAX_REGIONS];
+struct recipe recipe_parallel[MAX_REGIONS];
 static ompt_get_unique_id_t ompt_get_unique_id;
-static int nthreads;
+static int mode;
 
-static void
-on_ompt_callback_parallel_begin(
+/* this ompt function is called whenever parallel region begins (callback) */
+static void on_ompt_callback_parallel_begin(
   ompt_data_t *task_data,
   const ompt_frame_t *encounreting_task_frame,
   ompt_data_t *parallel_data,
   unsigned int requested_parallelism,
   int flags,
   const void *codeptr_ra)
-//  ompt_task_id_t parent_task_id,
-//  ompt_frame_t *parent_task_frame,
-//  ompt_parallel_id_t parallel_id,
-//  uint32_t requested_team_size,
-//  void *parallel_function,
-//  ompt_invoker_t invoker)
 {
-  if(nthreads==-1) {
+  /* mode -1 is running */
+  if(mode==-1) {
     if(recipe_parallel[region].codeptr==(void*)((int64_t)codeptr_ra-1)) 
       omp_set_num_threads(recipe_parallel[region].nworkers);
       region++;
   } else {
+  /* mode other than -1 is number of threads/workers for collect or scan 
+     time is measured on entry to parallel region */ 
     struct timeval timecheck;
     perfdata_parallel[rid].id=rid;
     perfdata_parallel[rid].workers=requested_parallelism;
@@ -71,26 +74,18 @@ on_ompt_callback_parallel_begin(
     perfdata_parallel[rid].begin=(long)timecheck.tv_sec * 1000000 + (long)timecheck.tv_usec;
     omp_set_num_threads(requested_parallelism);  
   }
-//,*(int*)(task_data->ptr));
-  //if(task_data->value==0) {
-  //  task_data->value=(int)ompt_get_unique_id();
-  //  printf("parallel data: %d\n",(int)task_data->value); 
-  //}
-  //else  
-  //printf("id: %d\n",(int)ompt_get_unique_id());
-  //printf("addr %p\n",(void*)((int64_t)codeptr_ra-1));
-  
-//  printf("%" PRIu64 ": ompt_event_parallel_begin: parent_task_id=%" PRIu64 ", parent_task_frame=%p, parallel_id=%" PRIu64 ", requested_team_size=%" PRIu32 ", parallel_function=%p, invoker=%d\n", ompt_get_thread_id(), parent_task_id, parent_task_frame, parallel_id, requested_team_size, parallel_function, invoker);
 }
 
-static void
-on_ompt_callback_parallel_end(
+/* this ompt function is called whenever parallel region ends (callback) */
+static void on_ompt_callback_parallel_end(
   ompt_data_t *parallel_data,
   ompt_data_t *task_data,
   int flags,
   const void *codeptr_ra)
 {
-  if(nthreads!=-1) {
+  /* mode other than -1 is number of threads/workers for collect or scan
+     time is measured on exit from parallel region */
+  if(mode!=-1) {
     struct timeval timecheck;
     gettimeofday(&timecheck,NULL);
     perfdata_parallel[rid].end=(long)timecheck.tv_sec * 1000000 + (long)timecheck.tv_usec;
@@ -98,48 +93,49 @@ on_ompt_callback_parallel_end(
   } 
 }
 
-
+/* this function is called on initiatlisation of OpenMP */
 int ompt_initialize(
   ompt_function_lookup_t lookup,
   int initial_device_num,
   ompt_data_t* data)
 {
   int nworkers;
+  /* we set and register callbacks */
   ompt_set_callback_t ompt_set_callback = (ompt_set_callback_t) lookup("ompt_set_callback");
   ompt_get_unique_id = (ompt_get_unique_id_t) lookup("ompt_get_unique_id");
-  //printf("%f\n",*(double*)(data->ptr));
-  //printf("[INIT-tool] libomp init time: %f\n", omp_get_wtime()-*(double*)(data->ptr));
-  //*(double*)(data->ptr)=omp_get_wtime();
-  //printf("OMP initialised %d\n",*(int*)(data->ptr)); 
-  //ompt_set_callback(ompt_callback_parallel_begin, (ompt_callback_t) &on_ompt_event_parallel_begin);
   register_callback(ompt_callback_parallel_begin);
   register_callback(ompt_callback_parallel_end);
-  nworkers=*(int*)(data->ptr);
-  if(nworkers==-1) { 
+  /* mode is read */
+  mode=*(int*)(data->ptr);
+  /* mode -1 is running */ 
+  if(mode==-1) { 
     region=0;
   } else {
+  /* mode other than -1 is collect or scan */
+    nworkers=mode;
     omp_set_num_threads(nworkers);
     rid=0;
-    for(int i=0;i<128;i++) {
+    for(int i=0;i<MAX_REGIONS;i++) {
       perfdata_parallel[i].id=-1;
     }
   }
-
-  return 1; //success
+  return 1;
 }
 
-
+/* this function is called on exit from OpenMP */
 void ompt_finalize(ompt_data_t* data)
 {
-  int i;
-
-  fd=open(fifo,0666);  
-  write(fd,&rid,sizeof(int));
-  write(fd,perfdata_parallel,sizeof(struct perfdata)*rid);
-  close(fd);
+  /* if collecting or scanning write results back to pipe */
+  if(mode!=-1) {
+    int i;
+    fd=open(fifo,0666);  
+    write(fd,&rid,sizeof(int));
+    write(fd,perfdata_parallel,sizeof(struct perfdata)*rid);
+    close(fd);
+  }
 }
 
-
+/* this ompt function is called by OpenMP to initialize the tool */
 ompt_start_tool_result_t* ompt_start_tool(
   unsigned int omp_version,
   const char *runtime_version)
@@ -149,13 +145,15 @@ ompt_start_tool_result_t* ompt_start_tool(
   static int nteams;
   int fd;
   char buffer[128],str_buffer[128];
+  /* read mode from pipe */
   mkfifo(fifo,0666);
   fd=open(fifo,O_RDONLY);
-  read(fd,&nthreads,sizeof(int));
-  if(nthreads==-1) {
-    // run mode
+  read(fd,&mode,sizeof(int));
+  /* mode -1 is running */
+  if(mode==-1) {
     FILE *file;
     int fblen;
+    /* open the recipe file and read */
     read(fd,&fblen,sizeof(int));
     read(fd,buffer,fblen);
     file=fopen(buffer,"r");
@@ -171,14 +169,7 @@ ompt_start_tool_result_t* ompt_start_tool(
     } 
   } 
   close(fd);
-  /*
-  printf("Using %d threads\n",nthreads);
-  
-  nenv=getenv("OTB_NTEAMS");
-  if(nenv!=NULL) nteams=atoi(nenv);
-  else nteams=1;
-  time=omp_get_wtime(); */
-  static ompt_start_tool_result_t ompt_start_tool_result = {&ompt_initialize,&ompt_finalize,{.ptr=&nthreads}};
+  /* register init and finalise functions and pass mode parameter */
+  static ompt_start_tool_result_t ompt_start_tool_result = {&ompt_initialize,&ompt_finalize,{.ptr=&mode}};
   return &ompt_start_tool_result;
-  
 }
